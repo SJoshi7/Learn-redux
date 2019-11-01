4746a44c36f623008debbf7a6fce3fa716eabedb
Sjoshi7 <sjoshise7@gmail.com>
2019-11-01 05:56:36 +0530

    counter completed and refactored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useSelector,useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Lets learn redux!</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <button onClick={()=>dispatch(decrement())}>-</button>
+      {isLogged?<h2>IMPORTANT INFO</h2>:''}
     </div>
   );
 }
